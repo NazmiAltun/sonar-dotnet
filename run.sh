6aa944b4db5306aa1833f7fff0c61381e2cf45d3
Nazmi Altun <nazmialtun@windowslive.com>
2020-05-15 11:36:03 +0800

    use named env parameters instead

---
+++ run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -l
+
+set -eu
+
+echo "Installing dotnet-sonarscanner...."
+sh -c "dotnet tool install --global dotnet-sonarscanner --version 4.8.0"
+
+
+begin_cmd="/dotnet-sonarscanner begin \\
+    /k:\"${PROJECT_KEY}\" \\
+    /n:\"${PROJECT_NAME}\" \\
+    /o:\"${ORGANIZATION}\" \\
+    /d:sonar.host.url=\"${SONAR_SERVER_URL}\" \\
+    /version:\"${GITHUB_RUN_NUMBER}\" \\
+    /d:sonar.login=\"${SONAR_TOKEN:?Please set the SONAR_TOKEN environment variable.}\""
+
+if [ -n "${OPENCOVER_REPORT_PATH}" ]
+then
+    # sh -c "sed -i 's/\/home\/runner\/work\/${GITHUB_REPOSITORY##*/}\/${GITHUB_REPOSITORY##*/}\//\/github\/workspace\//g' ${OPENCOVER_REPORT_PATH}"
+    begin_cmd="$begin_cmd /d:sonar.cs.opencover.reportsPaths=\"${OPENCOVER_REPORT_PATH}\""
+fi
+
+if [ -n "${CPD_EXCLUSIONS}" ]
+then
+    begin_cmd="$begin_cmd /d:sonar.cpd.exclusions=\"${CPD_EXCLUSIONS}\""
+fi
+
+if [ -n "${PR_KEY}" ]
+then
+    begin_cmd="$begin_cmd /d:sonar.pullrequest.github.repository=\"${GITHUB_REPOSITORY}\" \\
+    /d:sonar.pullrequest.github.branch=\"${GITHUB_REF#refs/heads/}\" \\
+    /d:sonar.pullrequest.github.key=\"${PR_KEY}\""
+fi
+
+sh -c "$begin_cmd"
+sh -c "dotnet build -c Release ${SOLUTION_PATH}"
+sh -c "/dotnet-sonarscanner end /d:sonar.login=\"${SONAR_TOKEN}\""
